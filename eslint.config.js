ac4cbfc25c71e554d0b133c9645d95107c08f86b
neoandmatrix <tusharannand@gmail.com>
2024-09-21 19:41:18 +0530

    add file naming convenction

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,43 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier';
+import checkFile from 'eslint-plugin-check-file';
 
 export default tseslint.config({
-  languageOptions : {
-    parserOptions : {
-      project:true,
-      tsconfigRootDir:import.meta.dirname,
-    },
+  languageOptions: {
+    parserOptions: {
+      project: true,
+      tsconfigRootDir: import.meta.dirname
+    }
   },
-  files:["**/*.ts"],
-  extends:[
+  files: ['**/*.ts'],
+   plugins: {
+      'check-file': checkFile,
+    },
+  extends: [
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     eslintConfigPrettier
   ],
+  rules:{
+    semi: "error",
+    "prefer-arrow-callback": ["error"],
+    "prefer-template": ["error"],
+    "check-file/filename-naming-convention": [
+      "error",
+      {
+        "**/*.ts": "SNAKE_CASE"
+      },
+      {
+        "ignoreMiddleExtensions": true
+      }
+    ],
+    "check-file/folder-naming-convention": [
+      "error",
+      {
+        "src/**": "KEBAB_CASE"
+      }
+    ]
+  }
 });
-
