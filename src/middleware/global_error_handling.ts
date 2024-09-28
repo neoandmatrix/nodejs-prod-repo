c3a64c818f40b257e62f09c257d4db21870c4efa
neoandmatrix <tusharannand@gmail.com>
2024-09-29 01:29:46 +0530

    some fixes

--- src/middleware/global_error_handling.ts
+++ src/middleware/global_error_handling.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpErrorType } from '../types/types.js';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (
   error: HttpErrorType,
   _: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   __: NextFunction
 ) => {
   res.status(error.statusCode).json(error);
